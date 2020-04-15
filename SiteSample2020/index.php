1bec1d59113d756ad58f97a74145915e7393fc35
Sir Mordred <55238630+Sir-Mordred@users.noreply.github.com>
2020-04-14 21:29:02 -0300

    Add files via upload

--- SiteSample2020/index.php
+++ SiteSample2020/index.php
@@ -9,18 +9,18 @@ Profa. Cristina V. P. B. Souza
 Novembro/2018
 *** Para o correto funcionamento do CSS, o equipamento precisa de internet ***
 ---------------------------------------------------------------------------------->
-	<title>IE - Instituição de Ensino</title>
+	<title>AKIM PNEUS</title>
 	<link rel="icon" type="image/png" href="imagens/IE_favicon.png"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<style>
 		.w3-theme {
 			color: #ffff !important;
-			background-color: #380077 !important
+			background-color: lightblue !important
 		}
 
 		.w3-code {
-			border-left: 4px solid #380077
+			border-left: 4px solid lightblue
 		}
 
 		.myMenu {
@@ -42,12 +42,12 @@ Novembro/2018
 		<div class="w3-main w3-container" style="margin-left:270px;margin-top:117px;">
 
 			<div class="w3-panel w3-padding-large w3-card-4 w3-light-grey">
-				<h1 class="w3-jumbo">Controle Acadêmico</h1>
-				<p class="w3-xlarge">Cadastros de Professores e Disciplinas</p>
+				<h1 class="w3-jumbo">Estoque</h1>
+				<p class="w3-xlarge">Cadastros de Produtos</p>
 
-				<img src="imagens/cadastro.jpg" class="w3-circle">
+				<img src="imagens/pneu_1.png" class="w3-circle" width="200" height="200">
 
-				<img src="imagens/quadro.jpg" class="w3-circle">
+				<img src="imagens/pneu_2.png" class="w3-circle" width="200" height="200">
 
 
 				<footer class="w3-panel w3-padding-32 w3-card-4 w3-light-grey w3-center w3-opacity">
--- SiteSample2020/profContratar.php
+++ SiteSample2020/profContratar.php
@@ -15,11 +15,11 @@
     <style>
         .w3-theme {
             color: #ffff !important;
-            background-color: #380077 !important
+            background-color: lightblue !important
         }
 
         .w3-code {
-            border-left: 4px solid #380077
+            border-left: 4px solid lightblue
         }
 
         .myMenu {
@@ -35,7 +35,7 @@
 <div class="w3-main w3-container" style="margin-left:270px;margin-top:117px;">
 
     <div class="w3-panel w3-padding-large w3-card-4 w3-light-grey">
-        <h1 class="w3-xlarge">Cadastro de Professores</h1>
+        <h1 class="w3-xlarge">Registro de Entrada de Produtos</h1>
 
         <p class="w3-large">
             <div class="w3-code cssHigh notranslate">
@@ -52,7 +52,7 @@
 
                 <div class="w3-responsive w3-card-4">
                     <div class="w3-container w3-theme">
-                        <h2>Informe os dados do novo do Professor</h2>
+                        <h2>Informe os dados do Produto</h2>
                     </div>
                     <form class="w3-container" action="ProfContratar_exe.php" method="post" onsubmit="return check(this.form)">
 						<input type="hidden" id="acaoForm" name="acaoForm" value="Contratar">
@@ -61,28 +61,28 @@
 						<input class="w3-input w3-border w3-light-grey" name="Nome" type="text" pattern="[a-zA-Z\u00C0-\u00FF ]{10,100}$"
 							   title="Nome entre 10 e 100 letras." required></p>
 						<p>
-						<label class="w3-text-deep-purple"><b>Celular</b></label>
+						<label class="w3-text-deep-purple"><b>ID</b></label>
 						<input class="w3-input w3-border w3-light-grey " name="Celular" type="text"
-							   pattern="\([0-9]{2}\)[0-9]{4,6}-[0-9]{3,4}$" title="(11)1111-1111." required></p>
+							   pattern="\([0-9]{8}\)$" title="11111111." required></p>
 						<p>
-						<label class="w3-text-deep-purple"><b>Data de Nascimento</b></label>
+						<label class="w3-text-deep-purple"><b>Data de Fabricacao</b></label>
 						<input class="w3-input w3-border w3-light-grey" name="DataNasc" type="date"
 							   pattern="((0[1-9])|([1-2][0-9])|(3[0-1]))\/((0[1-9])|(1[0-2]))\/((19|20)[0-9][0-9])"
 							   title="Formato: dd/mm/aaaa."></p>
 						<p>
-						<label class="w3-text-deep-purple"><b>Login</b></label>
-						<input class="w3-input w3-border w3-light-grey" name="Login" type="text"
-							   pattern="[a-zA-Z]{2,20}.[a-zA-Z]{2,20}" title="Formato: nome.sobrenome" required></p>
+						<label class="w3-text-deep-purple"><b>Data de Compra</b></label>
+						<input class="w3-input w3-border w3-light-grey" name="Login" type="date"
+							    pattern="((0[1-9])|([1-2][0-9])|(3[0-1]))\/((0[1-9])|(1[0-2]))\/((19|20)[0-9][0-9])"
+                               title="Formato: dd/mm/aaaa."></p>
 						<p>
-						<label class="w3-text-deep-purple"><b>Senha</b></label>
+						<label class="w3-text-deep-purple"><b>Marca</b></label>
 						<input class="w3-input w3-border w3-light-grey" name="Senha" type="password"
-							   pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}"
-							   title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres" required></p>
+							  title="Marca do produto" required></p>
 						<p>
-						<label class="w3-text-deep-purple"><b>Confirma Senha</b></label>
+						<label class="w3-text-deep-purple"><b>Quantidade</b></label>
 						<input class="w3-input w3-border w3-light-grey" name="Senha2" type="password"
-							   pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}"
-							   title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres" required> </p> 
+							   pattern="\([0-9]{5}\)$" title="Quantidade de produtos" required></p>
+							   
 						<p>
 						<input type="submit" value="Registrar" class="w3-btn w3-theme" >
 						<input type="button" value="Cancelar" class="w3-btn w3-theme" onclick="window.location.href='.'"></p>
