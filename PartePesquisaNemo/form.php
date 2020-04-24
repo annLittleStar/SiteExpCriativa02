7e12ffa999b01429e9f61f5546defc633827413a
victornemo <moreira.negrelli@pucpr.edu.br>
2020-04-24 17:40:00 -0300

    PartePesquisa

---
+++ PartePesquisaNemo/form.php
@@ -0,0 +1,19 @@
+<h1>Pesquisar Pneus</h1>
+
+<form method="POST" action="pesquisar.php">
+
+<!--
+Pesquisar por:
+<select>
+<option value="1">Marca</option>
+<option value="2">Modelo</option>
+<option value="3">Aro</option>
+<option value="4">Altura</option>
+<option value="5">Largura</option>
+</select>
+-->
+
+Pesquisar: <input type="text" name="pesquisar" placeholder="PESQUISAR">
+<input type="submit" value="ENVIAR">
+
+</form>
