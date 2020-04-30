21695885a117eb2c85e510b498059b3330923870
Fuyu-ko <55238889+Gabriel-Barboza@users.noreply.github.com>
2020-04-30 17:55:21 -0300

    Pesquisar - front-end
    Tbm tirei o fundo da imagem de saída

--- SiteProjeto/paginas/estoqueListar.php
+++ SiteProjeto/paginas/estoqueListar.php
@@ -26,6 +26,25 @@ Equipe: Ana Schran, Gabriel Barboza, Lohan Akim e Victor Negrelli
         .myMenu {
             margin-bottom: 150px
         }
+        input[type=submit]{
+            background-color: royalblue;
+            border-style: 0.1px solid;
+            border-color: royalblue;
+            color: white;
+            padding-top: 2.7px;
+            padding-bottom: 2.7px;
+        }
+        input[type=submit]:hover{
+            background-color: lightblue;
+            border-color: lightblue;
+            cursor: pointer;
+        }
+        input[type=text]{
+            border-style: 0.1px solid;
+            border-color: royalblue;
+            padding: 3px;
+            padding-left: 2%;
+        }     
     </style>
 </head>
 <body onload="w3_show_nav('menuEstoque')">
