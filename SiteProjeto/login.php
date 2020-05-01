07f527576be246d27ec72c9392d6e3596d97a80d
Sir Mordred <55238630+Sir-Mordred@users.noreply.github.com>
2020-05-01 18:07:43 -0300

    Add files via upload

--- SiteProjeto/login.php
+++ SiteProjeto/login.php
@@ -1,34 +1,26 @@
-<!-------------------------------------------------------------------------------
-PROJETO DE EXPERIÊNCIA CRIATIVA 02:
-SEA+
-PUCPR
-ENGENHARIA DE SOFTWARE
-
-Equipe: Ana Schran, Gabriel Barboza, Lohan Akim e Victor Negrelli
----------------------------------------------------------------------------------->
 <?php
 session_start();
 include('conexao.php'); 
 
-if(empty($_POST['login']) || empty($_POST['senha'])){
+if(empty($_POST['usuario']) || empty($_POST['senha'])){
 	header('Location: login.html');
 }
 
-$login = mysqli_real_escape_string($conexao, $_POST['login']);
+$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
-$query = "select id, login from funcionario where login = '{$login}' and senha = '{$senha}'";
+$query = "select id, login from funcionario where login = '{$usuario}' and senha = '{$senha}'";
 
 $result = mysqli_query($conexao, $query);
 
 $row = mysqli_num_rows($result);
 
 if ($row == 1) {
-	$_SESSION['login'] = $login;
-	header('Location: index.php');
+	$_SESSION['usuario'] = $usuario;
+	header('Location: paginas/index.php');
 	exit;
 }else{
 	header('Location: login.html');
 }
 
-?>
+ ?>
