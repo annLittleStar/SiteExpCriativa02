0904d19a46715a048600b03bba1312c701b10988
annLittleStar <ana.schran@pucpr.edu.br>
2020-05-04 22:40:08 -0300

    ajustes

--- SiteProjeto/paginas/cadastrarprod.php
+++ SiteProjeto/paginas/cadastrarprod.php
@@ -79,8 +79,8 @@ Equipe: Ana Schran, Gabriel Barboza, Lohan Akim e Victor Negrelli
                         <label class="w3-text-deep-purple"><b>Estado</b></label>
                         <input class="w3-input w3-border w3-light-grey" name="estado" type="text" tpattern="[a-zA-Z0-9\u00C0-\u00FF ]{3,4}$" title="Estado do Produto: bom ou ruim." required></p>
                         
-						<input type="submit" value="Cadastrar" class="w3-btn w3-theme" >
-						<input type="button" value="Cancelar" class="w3-btn w3-theme" onclick="window.location.href='.'"></p>
+						<input type="submit" value="Cadastrar" class="w3-btn w3-green" >
+						<input type="button" value="Cancelar" class="w3-btn w3-red" onclick="window.location.href='.'"></p>
 					</form>
 				</div>
 
