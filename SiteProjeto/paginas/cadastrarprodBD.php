c6edad5c5d79207ab0f9c6d8816ef4b5c026a3b7
Fuyu-ko <55238889+Gabriel-Barboza@users.noreply.github.com>
2020-05-05 15:42:59 -0300

    Cadastrar funcionando de novo

--- SiteProjeto/paginas/cadastrarprodBD.php
+++ SiteProjeto/paginas/cadastrarprodBD.php
@@ -77,7 +77,8 @@ Equipe: Ana Schran, Gabriel Barboza, Lohan Akim e Victor Negrelli
 			mysqli_query($conn,'SET character_set_results=utf8');
 
 		// Faz Select na Base de Dados
-		$sql = "INSERT INTO produto VALUES ('$id', '$nome','$marca', '$preco', '$tipo', '$estado')";
+		$sql = "INSERT INTO produto (id, nome, marca, preco, tipo, estado) 
+		VALUES ('$id', '$nome','$marca', '$preco', '$tipo', '$estado')";
 		echo "<div class='w3-responsive w3-card-4'>";
 		if ($result = mysqli_query($conn, $sql)) {
 			echo "Produto Cadastrado!";
