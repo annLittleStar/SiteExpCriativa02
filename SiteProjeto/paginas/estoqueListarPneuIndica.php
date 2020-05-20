0fcfaf9c7f8ca2f11fa3ced5ca98984444df50e1
Fuyu-ko <55238889+Gabriel-Barboza@users.noreply.github.com>
2020-05-20 11:20:23 -0300

    ...

--- SiteProjeto/paginas/estoqueListarPneuIndica.php
+++ SiteProjeto/paginas/estoqueListarPneuIndica.php
@@ -124,7 +124,6 @@ Equipe: Ana Schran, Gabriel Barboza, Lohan Akim e Victor Negrelli
                         <label class="w3-text-deep-purple"><b>Nome</b></label>
                         <input class="w3-input w3-border w3-light-grey" name="Nome" type="text" pattern="[a-zA-Z0-9\u00C0-\u00FF ]{4,100}$"
                         title="Nome do pneu entre 4 e 100 letras." value="<?php echo $row['nome']; ?>" required readonly></p>
-                        <p>
 
                                          <?php 
                         }
