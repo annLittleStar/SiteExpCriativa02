aa8b5c07bbf498dee4b0f81bcbadef83cbced06a
victornemo <moreira.negrelli@pucpr.edu.br>
2020-05-19 21:20:08 -0300

    duvida_a_esclarecer
    Pesquisa não funcionando

--- SiteProjeto/paginas/registroPneuDefPesquisa.php
+++ SiteProjeto/paginas/registroPneuDefPesquisa.php
@@ -96,6 +96,9 @@ Equipe: Ana Schran, Gabriel Barboza, Lohan Akim e Victor Negrelli
 
             // Faz Select na Base de Dados
             $sql = "SELECT id, nome, marca, qtdA, qtdR FROM produto JOIN pneuDef ON id = idPneu";
+            // ############
+            // Pesquisa não está funcionando!!
+            // ############
             echo "<div class='w3-responsive w3-card-4'>";
             if ($result = mysqli_query($conn, $sql)) {
                 echo "<table class='w3-table-all'>";
